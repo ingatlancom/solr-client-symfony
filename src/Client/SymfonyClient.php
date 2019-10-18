a9a6783bf7416ce786a5c8ef92d0df6b1b4fe76e
Egyed Gábor <egyed.gabor@ingatlan.com>
2019-10-18 14:40:59 +0200

    Add license headers

--- src/Client/SymfonyClient.php
+++ src/Client/SymfonyClient.php
@@ -2,6 +2,15 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of Solr Client Symfony package.
+ *
+ * (c) ingatlan.com Zrt. <fejlesztes@ingatlan.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace iCom\SolrClient\Client;
 
 use iCom\SolrClient\Client;
